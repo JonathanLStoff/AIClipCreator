7655c434b3f7a02bca72000eed37979447074e95
Nerfbilly64 <jonstoffex@comcast.net>
2025-02-15 23:02:29 -0500

    rotate

--- clip_creator/video_edit.py
+++ clip_creator/video_edit.py
@@ -47,7 +47,7 @@ def edit_video(input_file, output_file, zoom=1.0, target_size=(720, 1280), start
     # Create a black background and composite the result on it.
     #background = ColorClip(size=target_size, color=(0, 0, 0), duration=cropped_clip.duration).with_fps(cropped_clip.fps)
     #final_clip = background.overlay(cropped_clip, position=("center", "center"))
-    text_commm = TextClip(font="Vercetti Regular/Vercetti-Regular.ttf", text=text, font_size=70, color='white', duration=cropped_clip.duration, stroke_color='black', stroke_width=5).with_position("top").rotate(randint(-10, 10))
+    text_commm = TextClip(font="Vercetti Regular/Vercetti-Regular.ttf", text=text, font_size=70, color='white', duration=cropped_clip.duration, stroke_color='black', stroke_width=5).with_position("top").rotated(randint(-10, 10))
     final_clip = CompositeVideoClip([cropped_clip.with_position("center"), text_commm], size=target_size, bg_color=(0, 0, 0))
     
     final_clip.write_videofile(output_file.replace(".mp4", "_final.mp4"), codec="libx264")
