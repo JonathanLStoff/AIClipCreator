e47a0813a4b1c09ca87625c51f599d003f460b9b
Nerfbilly64 <jonstoffex@comcast.net>
2025-03-08 15:29:56 -0500

    emulator -avd Medium_Phone_API_35

--- script/android.sh
+++ script/android.sh
@@ -1,3 +1,8 @@
+MACOS:
+/Users/jonathanstoff/Library/Android/sdk/emulator/emulator -avd Medium_Phone_API_35
+
+WINDOWS:
+emulator -avd Medium_Phone_API_35
 
 sdkmanager "system-images;android-35;google_apis;x86_64"
 avdmanager -v create avd -p D:/vbox -n ClipEM -k "system-images;android-35;google_apis;x86_64"
