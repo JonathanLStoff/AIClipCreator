dbf4bb73c7be8a7e47740438a497c5088670a6e2
Jonathan Stoff <stoffaudio@gmail.com>
2025-03-08 15:01:07 -0500

    script

---
+++ script/android.sh
@@ -0,0 +1,4 @@
+
+sdkmanager "system-images;android-35;google_apis;x86_64"
+avdmanager -v create avd -p D:/vbox -n ClipEM -k "system-images;android-35;google_apis;x86_64"
+avdmanager create avd -n $ANDROID_AVD_NAME -k "system-images;android-$ANDROID_API_LEVEL;default;x86_64" --force
